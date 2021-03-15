e8d6ffc6b5e66a014d8a9eb9bab100eda0aa754b
Rick <dirklo@gmail.com>
2021-03-15 17:46:07 +0100

    commit tree 165c8df3ef2e6936835413edf769f2ecda407493

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+const dodger = document.querySelector('#dodger')
+document.addEventListener('keydown', (e) => {
+    if (e.key == "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key == "ArrowRight") {
+        moveDodgerRight();
+    } 
+});
+
+function moveDodgerLeft() {
+    dodger.style.left = parseInt(dodger.style.left) - 5 + 'px'
+}
+function moveDodgerRight() {
+    dodger.style.left = parseInt(dodger.style.left) + 5 + 'px'
+}
